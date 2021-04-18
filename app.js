abe7b1461be1238f4e7c8082a3c6ad7856ef5b5e
Michael Knowlton <mknowlton89@gmail.com>
2021-04-18 16:58:06 -0400

    Built out the first inquirer flow.

--- app.js
+++ app.js
@@ -16,8 +16,46 @@ const connection = mysql.createConnection({
     database: 'employee_cms_db',
 });
 
+const addDepartment = () => {
+    console.log('You added a new department!');
+}
+
+const quit = () => {
+    console.log('Bye!');
+    connection.end();
+}
+
+const startProgram = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'chooseType',
+                message: 'Would you like to do?',
+                choices: ['Add a Department', 'Add a Role', 'Add an Employee', 'View all Departments', 'View all Roles', 'View all Employees', 'Update Employee Roles', 'Exit'],
+            }
+        ])
+        .then((answer) => {
+            if (answer.chooseType === "Add a Department") {
+                addDepartment();
+            } else if (answer.chooseType === "Add a Role") {
+                addRole();
+            } else if (answer.chooseType === "Add an Employee") {
+                addEmployee();
+            } else if (answer.chooseType === "View all Departments") {
+                viewDepartments();
+            } else if (answer.chooseType === "View all Roles") {
+                viewRoles();
+            } else if (answer.chooseType === "View all Employees") {
+                viewEmployees();
+            } else {
+                quit();
+            }
+        })
+};
+
 connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}\n`);
-    // startProgram();
+    startProgram();
 });
