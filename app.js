4fca932cd0f06d24e35e1f4a3d1c1f8335dd8cf8
Michael Knowlton <mknowlton89@gmail.com>
2021-04-18 17:24:25 -0400

    Got the addRole and addDepartment fully wired up. The addEmployee is almost complete, just need to add an if statement in the event the employee does not have a manager.

--- app.js
+++ app.js
@@ -17,8 +17,106 @@ const connection = mysql.createConnection({
 });
 
 const addDepartment = () => {
-    console.log('You added a new department!');
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'dept_name',
+                message: 'What department would you like to add?',
+            }
+        ])
+        .then((answer) => {
+            let dept_name = answer.dept_name;
+            connection.query(`INSERT INTO department (dept_name) VALUES ('${dept_name}')`, (err, res) => {
+                if (err) throw err;
+                console.log(`${dept_name} was added!`);
+            })
+        })
 }
+
+const addRole = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is the title of the role that you would like to add?',
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'What is the salary of the role that you would like to add?',
+            },
+            {
+                type: 'input',
+                name: 'department_id',
+                message: 'What is the ID of the department this role is in?',
+            },
+            {
+                type: 'input',
+                name: 'dept_name',
+                message: 'What is the name of the department this role is in?',
+            },
+        ])
+        .then((answer) => {
+            let title = answer.title;
+            let salary = answer.salary;
+            let department_id = answer.department_id;
+            let dept_name = answer.dept_name;
+            connection.query(`INSERT INTO role (title, salary, department_id, dept_name) VALUES ('${title}', ${salary}, ${department_id}, '${dept_name}')`, (err, res) => {
+                if (err) throw err;
+                console.log(`${title} was added!`);
+            })
+        })
+}
+
+const addEmployee = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'first_name',
+                message: "What is the new employee's first name?",
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: "What is the new employee's last name?",
+            },
+            {
+                type: 'input',
+                name: 'role_id',
+                message: 'What is the ID of the role this employee will have?',
+            },
+            {
+                type: 'input',
+                name: 'manager_id',
+                message: "What is the ID of this employee's manager?",
+            },
+        ])
+        .then((answer) => {
+            let first_name = answer.first_name;
+            let last_name = answer.last_name;
+            let role_id = answer.role_id;
+            let manager_id = answer.manager_id;
+
+            // console.log(manager_id);
+
+            if (!manager_id) {
+                connection.query(`INSERT INTO employee (first_name, last_name, role_id) VALUES ('${first_name}', '${last_name}', ${role_id})`, (err, res) => {
+                    if (err) throw err;
+                    console.log(`${first_name} was added without a manager!`);
+                })
+            } else {
+                connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${first_name}', '${last_name}', ${role_id}, ${manager_id})`, (err, res) => {
+                    if (err) throw err;
+                    console.log(`${first_name} was added with a manager!`);
+                })
+            }
+        })
+}
+
+
 
 const quit = () => {
     console.log('Bye!');
